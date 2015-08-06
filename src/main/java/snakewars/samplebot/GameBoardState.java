61eaa2dab15675dd1943d9fe73b2ba4c62ed1cf3
Mateusz Boś <mateuszbos@gmail.com>
2015-08-06 08:15:00 +0200

    Final version

--- src/main/java/snakewars/samplebot/GameBoardState.java
+++ src/main/java/snakewars/samplebot/GameBoardState.java
@@ -1,5 +1,8 @@
 package snakewars.samplebot;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -28,9 +31,56 @@ public class GameBoardState {
     }
 
     public Set<PointDTO> getOccupiedCells() {
-        return Stream.concat(gameState.getWalls().stream(),
-                             gameState.getSnakes().stream().flatMap(snake -> snake.getCells().stream()))
-                     .collect(Collectors.toSet());
+        return getOccupiedCells(null);
+    }
+
+    public Set<PointDTO> getOccupiedCells(String mySnakeId) {
+
+        final Set<PointDTO> collect = Stream.concat(gameState.getWalls().stream(),
+                                                    gameState.getSnakes().stream().flatMap(snake -> snake.getCells().stream()))
+                                            .collect(Collectors.toSet());
+        collect.addAll(getFoodCellsGraniczaceWithSnakeHead());
+        //collect.addAll(getSnakeHeads(mySnakeId));
+        return collect;
+    }
+
+    private Collection<? extends PointDTO> getSnakeHeads(String mySnakeId) {
+        Set<PointDTO> adjecentToHeads = new HashSet<>();
+        final Set<PointDTO> heads = gameState.getSnakes().stream().filter(s -> !s.getId().equals(mySnakeId)).map(snakeDTO -> snakeDTO.getHead())
+                                             .collect(Collectors.toSet());
+        for(PointDTO head : heads) {
+            adjecentToHeads.add(new PointDTO(head.getX() - 1, head.getY() - 1));
+            adjecentToHeads.add(new PointDTO(head.getX() - 1, head.getY()));
+            adjecentToHeads.add(new PointDTO(head.getX() - 1, head.getY() + 1));
+            adjecentToHeads.add(new PointDTO(head.getX(), head.getY() + 1));
+            adjecentToHeads.add(new PointDTO(head.getX() + 1, head.getY() + 1));
+            adjecentToHeads.add(new PointDTO(head.getX() + 1, head.getY()));
+            adjecentToHeads.add(new PointDTO(head.getX() + 1, head.getY() - 1));
+            adjecentToHeads.add(head);
+        }
+        return adjecentToHeads;
+    }
+
+    private Set<PointDTO> getFoodCellsGraniczaceWithSnakeHead() {
+        final List<PointDTO> food = gameState.getFood();
+        final Set<PointDTO> heads = gameState.getSnakes().stream().map(snake -> snake.getHead()).collect(Collectors.toSet());
+
+        return food.stream().filter(foodDTO -> hasAnySnakeHaedAdjecent(foodDTO, heads)).collect(Collectors.toSet());
+    }
+
+    private boolean hasAnySnakeHaedAdjecent(PointDTO food, Set<PointDTO> heads) {
+        for(PointDTO head : heads) {
+            final int deltaX = delta(head.getX(), food.getX());
+            final int deltaY = delta(head.getY(), food.getY());
+            if((deltaX == 1 && deltaY == 0) || (deltaY == 1 && deltaX == 0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int delta(int p1, int p2) {
+        return Math.abs(p1 - p2);
     }
 
     public Set<PointDTO> getFoodCells() {
